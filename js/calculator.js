c7ac9e8b27a92e2d4453a74ecdfe56794ae93da9
shrfrn <shrfrn@gmail.com>
2023-11-15 11:16:48 +0200

    Implement onNumClick()

--- js/calculator.js
+++ js/calculator.js
@@ -1,7 +1,12 @@
 'use strict'
 
+var num1 = 0
+var num2 = 0
+var operator = ''
+
 function onNumClick(elCell) {
-    console.log(elCell.innerText)
+    if(!num1) num1 = +elCell.innerText
+    else if(operator && !num2) num2 = +elCell.innerText
 }
 
 function onOperatorClick(elCell) {
